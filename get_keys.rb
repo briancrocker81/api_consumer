fce06aed703ea5a2505752dde2ce0e2d6cc42565
Brian Crocker <brian.crocker@vualto.com>
2017-03-15 21:21:31 +0000

    added xml builder with nokogiri

--- get_keys.rb
+++ get_keys.rb
@@ -2,55 +2,84 @@ require 'rest-client'
 require 'nokogiri'
 require 'active_support'
 require 'active_support/core_ext'
+require 'base64'
 
 class GenerateKey
 
   def initialize
-    url         = "http://localhost:8989/LiveDrmService/LiveDrmService.asmx"
-    auth = "bGl2ZWRybTpsaXZlZHJt"
-    body = '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:liv="http://man.entriq.net/livedrmservice/"><soap:Header><liv:LiveDrmServiceHeader m_sUsername="admin@unifieds.com" m_sPassword="unifieds" KMSUsername="livedrm" KMSPassword="livedrm"/></soap:Header><soap:Body><liv:GenerateKeys><liv:accountId>default</liv:accountId><liv:contentId>1007</liv:contentId><liv:protectionSystem><liv:string>IrdetoProtection</liv:string></liv:protectionSystem></liv:GenerateKeys></soap:Body></soap:Envelope>'
+  end
 
+  def get_key(content_id)
+    content_id  = content_id
+    url         = "http://localhost:8989/LiveDrmService/LiveDrmService.asmx"
+    auth        = "bGl2ZWRybTpsaXZlZHJt"
+    account_id  = "default"
+    protection  = "IrdetoProtection"
+    m_sUsername = "admin@unifieds.com"
+    m_sPassword = "unifieds"
+    kmsUsername = "livedrm"
+    kmsPassword = "livedrm"
+    generate_body(content_id, url, auth, account_id, protection, m_sUsername, m_sPassword, kmsUsername, kmsPassword)
+  end
+
+  def generate_body(content_id, url, auth, account_id, protection, m_sUsername, m_sPassword, kmsUsername, kmsPassword)
+
+    namespaces  = {"xmlns:soap" => "http://www.w3.org/2003/05/soap-envelope", "xmlns:liv" => "http://man.entriq.net/livedrmservice/"}
+    header = {"m_sUsername" => "#{m_sUsername}", "m_sPassword" => "#{m_sPassword}", "KMSUsername" => "#{kmsUsername}", "KMSPassword" => "#{kmsPassword}"}
+    b = Nokogiri::XML::Builder.new
+
+    b[:soap].Envelope(namespaces) {
+      b[:soap].Header() {
+        b[:liv].LiveDrmServiceHeader(header)
+      }
+      b[:soap].Body {
+        b[:liv].GenerateKeys() {
+          b[:liv].accountId(account_id)
+          b[:liv].contentId(content_id)
+          b[:liv].protectionSystem(){b[:liv].string(protection)}
+        }
+      }
+    }
+
+    body = b.to_xml(:save_with => Nokogiri::XML::Node::SaveOptions::AS_XML | Nokogiri::XML::Node::SaveOptions::NO_DECLARATION).strip.to_s
 
     response = RestClient::Request.new({
-      method: :post,
-      url: url,
-      payload: body,
-      headers: { :Authentication => "Basic #{auth}", :content_type => "text/xml; charset=UTF-8" },
-    }).execute do |response|
+         method: :post,
+         url: url,
+         payload: t_body,
+         headers: { :Authentication => "Basic #{auth}", :content_type => "text/xml; charset=UTF-8" }
+     }).execute do |response|
       case response.code
-      when 500
-        puts "WARNING: -- Local error"
+        when 500
+          puts "WARNING: -- Local error"
           puts response.to_str
-      when 400
-        [ :error, response.to_str ]
+        when 400
+          [ :error, response.to_str ]
         when 200
-        [ :success, response.to_str ]
+          [ :success, response.to_str ]
           # puts "SUCCESS -- " + response
           convert_to_xml(response)
-      else
-        fail "Invalid response #{response.to_str} received."
+        else
+          fail "Invalid response #{response.to_str} received."
       end
     end
-
-    # response = RestClient::Request.execute(method: :post, url: url,
-    #                                        payload: body, headers: {:Authentication => "Basic #{auth}", :content_type => "text/xml; charset=UTF-8" })
-    # puts response
-
   end
 
   def convert_to_xml(response)
     doc = Nokogiri::XML.parse response
-    get_key(doc)
-
+    convert_key(doc)
   end
 
-  def get_key(doc)
+  def convert_key(doc)
     hash = Hash.from_xml(doc)
-    aes_key = hash["Keys"]["Key"]["ContentKey"]
-    puts aes_key
+    hash_key = hash["Keys"]["Key"]["ContentKey"]
+    aes_key = hash_key.unpack("m0").first.unpack("H*").first
+    details = { contentKey: aes_key, laurl: "drm2.tv.delta.nl" }.to_s
   end
 
 end
 
 key = GenerateKey.new
-puts key
+request = key.get_key("1007")
+
+puts request
