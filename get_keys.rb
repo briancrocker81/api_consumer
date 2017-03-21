c98d5a3e4bb48c9ee0e6b37325c4578e6a552233
Brian Crocker <brian.crocker@vualto.com>
2017-03-21 15:01:08 +0000

    Shortened the make body request and refactored the ENV variable names

--- get_keys.rb
+++ get_keys.rb
@@ -4,70 +4,62 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'base64'
 
-class GenerateKey
+module AesKey
+  class GenerateKey
 
-  def initialize;  end
+    def initialize;  end
 
-  def get_config(content_id)
-    content_id  = content_id
-    generate_body(content_id)
-  end
+    def get_config(content_id)
+      content_id  = content_id
+      generate_body(content_id)
+    end
 
-  def generate_body(content_id)
-  rescue Exception => e
-    puts "Could not load configuration options"
-  else
-    namespaces  = {"xmlns:soap" => "http://www.w3.org/2003/05/soap-envelope", "xmlns:liv" => "http://man.entriq.net/livedrmservice/"}
-    header = {"m_sUsername" => "#{ENV['m_sUsername']}", "m_sPassword" => "#{ENV['m_sPassword']}", "KMSUsername" => "#{ENV['kmsUsername']}", "KMSPassword" => "#{ENV['kmsUsername']}"}
-    b = Nokogiri::XML::Builder.new
-    b[:soap].Envelope(namespaces) {
-      b[:soap].Header() {
-        b[:liv].LiveDrmServiceHeader(header)
-      }
-      b[:soap].Body {
-        b[:liv].GenerateKeys() {
-          b[:liv].accountId(ENV['account_id'])
-          b[:liv].contentId(content_id)
-          b[:liv].protectionSystem(){b[:liv].string(ENV['protection'])}
+    def generate_body(content_id)
+      rescue Exception => e
+        puts "Could not load configuration options"
+      else
+        namespaces  = {"xmlns:soap" => "http://www.w3.org/2003/05/soap-envelope", "xmlns:liv" => "http://man.entriq.net/livedrmservice/"}
+        header = {"m_sUsername" => "#{ENV['M_S_USERNAME']}", "m_sPassword" => "#{ENV['M_S_PASSWORD']}", "KMSUsername" => "#{ENV['KMS_USERNAME']}", "KMSPassword" => "#{ENV['KMS_PASSWORD']}"}
+        b = Nokogiri::XML::Builder.new
+        b[:soap].Envelope(namespaces) {
+          b[:soap].Header() {
+            b[:liv].LiveDrmServiceHeader(header)
+          }
+          b[:soap].Body {
+            b[:liv].GenerateKeys() {
+              b[:liv].accountId(ENV['ACCOUNT_ID'])
+              b[:liv].contentId(content_id)
+              b[:liv].protectionSystem(){b[:liv].string(ENV['IRDETO_PROTECTION'])}
+            }
+          }
         }
-      }
-    }
-    body = b.to_xml(:save_with => Nokogiri::XML::Node::SaveOptions::AS_XML | Nokogiri::XML::Node::SaveOptions::NO_DECLARATION).strip.to_s
-    make_request(body)
-  end
+        body = b.to_xml(:save_with => Nokogiri::XML::Node::SaveOptions::AS_XML | Nokogiri::XML::Node::SaveOptions::NO_DECLARATION).strip.to_s
+        make_request(body)
+    end
 
-  def make_request(body)
-    response = RestClient::Request.new({
-                                           method: :post,
-                                           url: ENV['url'],
-                                           payload: body,
-                                           headers: { :Authentication => "Basic #{ENV['auth']}", :content_type => "text/xml; charset=UTF-8" }
-                                       }).execute do |response|
-      case response.code
-        when 500
-          puts "WARNING: -- Local error"
-          puts response.to_str
-        when 400
-          [ :error, response.to_str ]
-        when 200
-          [ :success, response.to_str ]
-          convert_key(response)
-        else
-          fail "Invalid response #{response.to_str} received."
+    def make_request(body)
+      response = RestClient::Request.execute(
+          method: :post,
+          url: ENV['IRDETO_URL'],
+          payload: body,
+          headers: { :Authentication => "Basic #{ENV['AUTHENTICATION']}", :content_type => "text/xml; charset=UTF-8" }
+      )
+      unless response.code == 200
+        raise "an error"
       end
+      convert_key(response)
+    end
+
+    def convert_key(response)
+      hash = Hash.from_xml(Nokogiri::XML.parse response)
+      hash_key_id = hash["Keys"]["Key"]["KeyId"]
+      hash_content_key = hash["Keys"]["Key"]["ContentKey"]
+      aes_key = hash_content_key.unpack("m0").first.unpack("H*").first
+      details = { contentKey: aes_key, laurl: "http://drm2.tv.delta.nl/keyfile/#{hash_key_id}" }
     end
   end
-
-  def convert_key(response)
-    hash = Hash.from_xml(Nokogiri::XML.parse response)
-    hash_key_id = hash["Keys"]["Key"]["KeyId"]
-    hash_content_key = hash["Keys"]["Key"]["ContentKey"]
-    aes_key = hash_content_key.unpack("m0").first.unpack("H*").first
-    details = { contentKey: aes_key, laurl: "http://drm2.tv.delta.nl/keyfile/#{hash_key_id}" }
-  end
-
 end
 
-key = GenerateKey.new.get_config(1007)
+key = AesKey::GenerateKey.new.get_config(1007)
 # request = key.get_config(1007)
 puts key
