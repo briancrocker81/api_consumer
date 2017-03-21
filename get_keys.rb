e864a487e2c1686cfd2530323524c12ac1907fd5
Brian Crocker <brian.crocker@vualto.com>
2017-03-21 09:13:48 +0000

    separated the generate body method out into two methods generate body and make request

--- get_keys.rb
+++ get_keys.rb
@@ -16,7 +16,6 @@ class GenerateKey
   end
 
   def generate_body(content_id, config)
-
     begin
       data_hash = JSON.parse(config)
     rescue JSON::ParserError => e
@@ -24,7 +23,6 @@ class GenerateKey
     else
       namespaces  = {"xmlns:soap" => "http://www.w3.org/2003/05/soap-envelope", "xmlns:liv" => "http://man.entriq.net/livedrmservice/"}
       header = {"m_sUsername" => "#{data_hash['irdeto']['m_sUsername']}", "m_sPassword" => "#{data_hash['irdeto']['m_sPassword']}", "KMSUsername" => "#{data_hash['irdeto']['kmsUsername']}", "KMSPassword" => "#{data_hash['irdeto']['kmsUsername']}"}
-
       b = Nokogiri::XML::Builder.new
       b[:soap].Envelope(namespaces) {
         b[:soap].Header() {
@@ -39,25 +37,28 @@ class GenerateKey
         }
       }
       body = b.to_xml(:save_with => Nokogiri::XML::Node::SaveOptions::AS_XML | Nokogiri::XML::Node::SaveOptions::NO_DECLARATION).strip.to_s
+      make_request(body, data_hash)
+    end
+  end
 
-      response = RestClient::Request.new({
-        method: :post,
-        url: data_hash['irdeto']['url'],
-        payload: body,
-        headers: { :Authentication => "Basic #{data_hash['irdeto']['auth']}", :content_type => "text/xml; charset=UTF-8" }
-      }).execute do |response|
-        case response.code
-          when 500
-            puts "WARNING: -- Local error"
-            puts response.to_str
-          when 400
-            [ :error, response.to_str ]
-          when 200
-            [ :success, response.to_str ]
-            convert_key(response)
-          else
-            fail "Invalid response #{response.to_str} received."
-        end
+  def make_request(body, data_hash)
+    response = RestClient::Request.new({
+      method: :post,
+      url: data_hash['irdeto']['url'],
+      payload: body,
+      headers: { :Authentication => "Basic #{data_hash['irdeto']['auth']}", :content_type => "text/xml; charset=UTF-8" }
+    }).execute do |response|
+      case response.code
+        when 500
+          puts "WARNING: -- Local error"
+          puts response.to_str
+        when 400
+          [ :error, response.to_str ]
+        when 200
+          [ :success, response.to_str ]
+          convert_key(response)
+        else
+          fail "Invalid response #{response.to_str} received."
       end
     end
   end
@@ -68,11 +69,10 @@ class GenerateKey
     hash_content_key = hash["Keys"]["Key"]["ContentKey"]
     aes_key = hash_content_key.unpack("m0").first.unpack("H*").first
     details = { contentKey: aes_key, laurl: "http://drm2.tv.delta.nl/keyfile/#{hash_key_id}" }
-    puts details.to_s
   end
 
 end
 
 key = GenerateKey.new.get_config(1007)
 # request = key.get_config(1007)
-# puts request
+puts key
